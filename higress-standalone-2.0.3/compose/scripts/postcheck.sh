822bb610fa74e6b92cba2b77586b104e6b26baee
herui.gh <>
2024-11-11 19:07:20 +0800

    .

--- higress-standalone-2.0.3/compose/scripts/postcheck.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-echo "All good!"
--- higress-standalone-2.0.3/src/apiserver/pkg/converter/converters.go
+++
@@ -1,9 +0,0 @@
-package converter
-
-import (
-	"k8s.io/apimachinery/pkg/runtime"
-)
-
-func RegisterConverters(scheme *runtime.Scheme) {
-	registerGatewayApiConverters(scheme)
-}
